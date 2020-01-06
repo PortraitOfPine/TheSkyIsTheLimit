3c65446345b1271831847602aeef25378546875e
Bruno Roy <brusi_roy@hotmail.com>
2020-01-05 20:54:35 -0800

    Add clock display script
    Add a clock display that currently only shows the time since the game
    started.

---
+++ Assets/Scripts/ClockDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class ClockDisplay : MonoBehaviour
+{
+    public Text countdown; //UI Text Object
+
+    void Start()
+    {
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+
+    void Update()
+    {
+        countdown.text = ("" + Time.time); //Showing the Score on the Canvas
+    }
+}
