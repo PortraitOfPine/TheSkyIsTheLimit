e0d62820d3e61c6832eb53beaa19c1d076fa89c3
Simon Roy <simon.roy5219@gmail.com>
2020-01-16 19:52:52 -0500

    add input field for timescale and input to hide the UI
    You can now enter the timescale for the simulation in the input field. The whole UI can be hidden by pressing the 'e' key.

--- Assets/Scripts/ClockDisplay.cs
+++ Assets/Scripts/ClockDisplay.cs
@@ -7,19 +7,34 @@ using System.Threading;
 public class ClockDisplay : MonoBehaviour
 {
     public Text countdown; //UI Text Object
+    public Text timeScaleDisplay; //UI Text Object
 
     void Start()
     {
-        Time.timeScale = 1; //Just making sure that the timeScale is right
+        ChangeTimeScale(1); //Just making sure that the timeScale is right
     }
 
     void Update()
     {
-        countdown.text = ("" + Time.time); //Showing the Score on the Canvas
+        float hours = Mathf.Floor(Time.time / (60 * 60));
+        float minutes = Mathf.Floor((Time.time % (60*60)) / (60));
+        float seconds = Mathf.RoundToInt(Time.time % (60));
+        countdown.text = ("" + hours+ "h " + minutes + "m " + seconds + "s"); //Showing the Score on the Canvas
+
     }
 
+    //Update the timescale and the display
     public void ChangeTimeScale(float timeScaleValue)
     {
         Time.timeScale = timeScaleValue;
+        timeScaleDisplay.text = timeScaleValue.ToString();
+    }
+
+    //Change the timescale following the end of editing
+    public void InputFieldTime(InputField input)
+    {
+       
+        ChangeTimeScale(float.Parse(input.text));
+
     }
 }
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public CanvasGroup clockUI;
+
+    public bool ShowUI { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowUI = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            if (ShowUI == true)
+            {
+                Hide();
+                ShowUI = false;
+            }
+            else
+            {
+                Show();
+                ShowUI = true;
+            }
+        }
+    }
+
+    void Hide() {
+        clockUI.alpha = 0f; //this makes everything transparent
+        clockUI.blocksRaycasts = false; //this prevents the UI element to receive input events
+    }
+
+    void Show() {
+        clockUI.alpha = 1f;
+        clockUI.blocksRaycasts = true;
+    }
+}
