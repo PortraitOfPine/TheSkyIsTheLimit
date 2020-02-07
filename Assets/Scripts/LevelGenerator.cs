fe3314fea327e5227a6c76d38906ee065e347c73
Simon Roy <simon.roy5219@gmail.com>
2020-02-06 23:45:53 -0500

    add terrain specific spawns

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour {
  
     [SerializeField]
+    private TreeGeneration treeGeneration;
+
+    [SerializeField]
     private int mapWidthInTiles, mapDepthInTiles;
  
     [SerializeField]
@@ -13,12 +16,21 @@ public class LevelGenerator : MonoBehaviour {
     void Start() {
         GenerateMap ();
     }
- 
     void GenerateMap() {
         // get the tile dimensions from the tile Prefab
         Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer> ().bounds.size;
         int tileWidth = (int)tileSize.x;
         int tileDepth = (int)tileSize.z;
+        
+        // calculate the number of vertices of the tile in each axis using its mesh
+        Vector3[] tileMeshVertices = tilePrefab.GetComponent<MeshFilter>().sharedMesh.vertices;
+        int tileDepthInVertices = (int)Mathf.Sqrt (tileMeshVertices.Length);
+        int tileWidthInVertices = tileDepthInVertices;
+ 
+        float distanceBetweenVertices = (float)tileDepth / (float)tileDepthInVertices;
+
+        // build an empty LevelData object, to be filled with the tiles to be generated
+        LevelData levelData = new LevelData (tileDepthInVertices, tileWidthInVertices, this.mapDepthInTiles, this.mapWidthInTiles);
  
         // for each Tile, instantiate a Tile in the correct position
         for (int xTileIndex = 0; xTileIndex < mapWidthInTiles; xTileIndex++) {
@@ -29,7 +41,61 @@ public class LevelGenerator : MonoBehaviour {
                     this.gameObject.transform.position.z + zTileIndex * tileDepth);
                 // instantiate a new Tile
                 GameObject tile = Instantiate (tilePrefab, tilePosition, Quaternion.identity) as GameObject;
+                // generate the Tile texture and save it in the levelData
+                TileData tileData = tile.GetComponent<TileGeneration> ().GenerateTile();
+                levelData.AddTileData (tileData, zTileIndex, xTileIndex);
             }
         }
+        // generate trees for the level
+        treeGeneration.GenerateTrees (this.mapDepthInTiles * tileDepthInVertices, this.mapWidthInTiles * tileWidthInVertices, distanceBetweenVertices, levelData);
+    }
+}
+
+// class to store all the merged tiles data
+public class LevelData {
+    private int tileDepthInVertices, tileWidthInVertices;
+
+    public TileData[,] tilesData;
+
+    public LevelData(int tileDepthInVertices, int tileWidthInVertices, int levelDepthInTiles, int levelWidthInTiles) {
+        // build the tilesData matrix based on the level depth and width
+        tilesData = new TileData[tileDepthInVertices * levelDepthInTiles, tileWidthInVertices * levelWidthInTiles];
+
+        this.tileDepthInVertices = tileDepthInVertices;
+        this.tileWidthInVertices = tileWidthInVertices;
+    }
+
+    public void AddTileData(TileData tileData, int tileZIndex, int tileXIndex) {
+        // save the TileData in the corresponding coordinate
+        tilesData [tileZIndex, tileXIndex] = tileData;
+    }
+
+    public TileCoordinate ConvertToTileCoordinate(int zIndex, int xIndex) {
+        // the tile index is calculated by dividing the index by the number of tiles in that axis
+        int tileZIndex = (int)Mathf.Floor ((float)zIndex / (float)this.tileDepthInVertices);
+        int tileXIndex = (int)Mathf.Floor ((float)xIndex / (float)this.tileWidthInVertices);
+        // the coordinate index is calculated by getting the remainder of the division above
+        // we also need to translate the origin to the bottom left corner
+        int coordinateZIndex = this.tileDepthInVertices - (zIndex % this.tileDepthInVertices) - 1;
+        int coordinateXIndex = this.tileWidthInVertices - (xIndex % this.tileDepthInVertices) - 1;
+ 
+        TileCoordinate tileCoordinate = new TileCoordinate (tileZIndex, tileXIndex, coordinateZIndex, coordinateXIndex);
+        return tileCoordinate;
+    }
+}
+
+
+// class to represent a coordinate in the Tile Coordinate System
+public class TileCoordinate {
+    public int tileZIndex;
+    public int tileXIndex;
+    public int coordinateZIndex;
+    public int coordinateXIndex;
+ 
+    public TileCoordinate(int tileZIndex, int tileXIndex, int coordinateZIndex, int coordinateXIndex) {
+        this.tileZIndex = tileZIndex;
+        this.tileXIndex = tileXIndex;
+        this.coordinateZIndex = coordinateZIndex;
+        this.coordinateXIndex = coordinateXIndex;
     }
 }
--- Assets/Scripts/TileGeneration.cs
+++ Assets/Scripts/TileGeneration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 //https://gamedevacademy.org/complete-guide-to-procedural-level-generation-in-unity-part-1/
 
@@ -11,14 +12,11 @@ public class TerrainType {
     public string name;
     public float height;
     public Color color;
+    public int index;
 }
-
-
 
 public class TileGeneration : MonoBehaviour {
  
-    [SerializeField]
-    private TerrainType[] terrainTypes;
 
     [SerializeField]
     NoiseGeneration noiseMapGeneration;
@@ -34,6 +32,9 @@ public class TileGeneration : MonoBehaviour {
  
     [SerializeField]
     private float mapScale;
+    
+    [SerializeField]
+    private TerrainType[] heightTerrainTypes;
  
     [SerializeField]
     private float heightMultiplier;
@@ -78,7 +79,7 @@ public class TileGeneration : MonoBehaviour {
         GenerateTile ();
     }
  
-    void GenerateTile() {
+    public TileData GenerateTile() {
         // calculate tile depth and width based on the mesh vertices
         Vector3[] meshVertices = this.meshFilter.mesh.vertices;
         int tileDepth = (int)Mathf.Sqrt(meshVertices.Length);
@@ -92,42 +93,49 @@ public class TileGeneration : MonoBehaviour {
         // calculate the offsets based on the tile position
         float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap(tileDepth, tileWidth, this.mapScale, offsetX, offsetZ, waves);
  
-        // generate a heightMap using noise
-        Texture2D tileTexture = BuildTexture(heightMap);
-        this.tileRenderer.material.mainTexture = tileTexture;
+        // build a Texture2D from the height map
+        TerrainType[,] chosenHeightTerrainTypes = new TerrainType[tileDepth, tileWidth];
+        Texture2D heightTexture = BuildTexture (heightMap, this.heightTerrainTypes, chosenHeightTerrainTypes);
+
+       
+        this.tileRenderer.material.mainTexture = heightTexture;
         UpdateMeshVertices(heightMap);
+        
+        TileData tileData = new TileData (heightMap,  chosenHeightTerrainTypes, this.meshFilter.mesh);
+
+        return tileData;
     }
  
-    private Texture2D BuildTexture(float[,] heightMap) {
-        int tileDepth = heightMap.GetLength(0);
-        int tileWidth = heightMap.GetLength(1);
- 
+    private Texture2D BuildTexture(float[,] heightMap, TerrainType[] terrainTypes, TerrainType[,] chosenTerrainTypes) {
+        int tileDepth = heightMap.GetLength (0);
+        int tileWidth = heightMap.GetLength (1);
+
         Color[] colorMap = new Color[tileDepth * tileWidth];
         for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
             for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
-                
                 // transform the 2D map index is an Array index
-                int colorIndex = zIndex * tileWidth+ xIndex;
-                float height= heightMap[zIndex, xIndex];
-                
+                int colorIndex = zIndex * tileWidth + xIndex;
+                float height = heightMap [zIndex, xIndex];
                 // choose a terrain type according to the height value
-                TerrainType terrainType = ChooseTerrainType (height);
+                TerrainType terrainType = ChooseTerrainType (height, terrainTypes);
                 // assign the color according to the terrain type
                 colorMap[colorIndex] = terrainType.color;
-            
+
+                // save the chosen terrain type
+                chosenTerrainTypes [zIndex, xIndex] = terrainType;
             }
         }
- 
+
         // create a new texture and set its pixel colors
         Texture2D tileTexture = new Texture2D (tileWidth, tileDepth);
         tileTexture.wrapMode = TextureWrapMode.Clamp;
         tileTexture.SetPixels (colorMap);
         tileTexture.Apply ();
- 
+
         return tileTexture;
     }
 
-    TerrainType ChooseTerrainType(float height) {
+    TerrainType ChooseTerrainType(float height, TerrainType[] terrainTypes) {
         // for each terrain type, check if the height is lower than the one for the terrain type
         foreach (TerrainType terrainType in terrainTypes) {
             // return the first terrain type whose height is higher than the generated one
@@ -136,5 +144,29 @@ public class TileGeneration : MonoBehaviour {
             }
         }
         return terrainTypes [terrainTypes.Length - 1];
+    }
+}
+
+
+// class to store all data for a single tile
+public class TileData {
+    public float[,]  heightMap;
+    //public float[,]  heatMap;
+    //public float[,]  moistureMap;
+    public TerrainType[,] chosenHeightTerrainTypes;
+    //public TerrainType[,] chosenHeatTerrainTypes;
+    //public TerrainType[,] chosenMoistureTerrainTypes;
+    //public Biome[,] chosenBiomes;
+    public Mesh mesh;
+ 
+    public TileData(float[,]  heightMap, TerrainType[,] chosenHeightTerrainTypes, Mesh mesh) {
+        this.heightMap = heightMap;
+        //this.heatMap = heatMap;
+        //this.moistureMap = moistureMap;
+        this.chosenHeightTerrainTypes = chosenHeightTerrainTypes;
+        //this.chosenHeatTerrainTypes = chosenHeatTerrainTypes;
+        //this.chosenMoistureTerrainTypes = chosenMoistureTerrainTypes;
+        //this.chosenBiomes = chosenBiomes;
+        this.mesh = mesh;
     }
 }
